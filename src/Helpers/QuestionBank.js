02d6bb9fa13f6006d8937d4b9f5c95b904ae185b
Praneshwar24 <praneshwar.prakasam24@gmail.com>
2021-11-26 17:04:38 +0530

    Coding a Quiz App in ReactJS

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+        prompt: "What is 24*12?",
+        optionA: "258",
+        optionB: "488",
+        optionC: "728",
+        optionD: "288",
+        answer: "D",
+    },
+    {
+        prompt: "How many valves does the heart have?",
+        optionA: "One",
+        optionB: "Two",
+        optionC: "Three",
+        optionD: "Four",
+        answer: "D",
+    },
+    {
+        prompt: "What is the smallest country in the world?",
+        optionA: "Vatican City",
+        optionB: "Myanmar",
+        optionC: "Monaco",
+        optionD: "Spain",
+        answer: "A",
+    },
+]
